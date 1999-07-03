bcf665c386b42cd78b88f557b0089d58a608f651
nakahiro <nakahiro>
1999-07-03 10:26:56 +0000

    Added COPYRIGHT notation.

--- TCPSocketPipe.rb
+++ TCPSocketPipe.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-RCS_ID = %q$Id: TCPSocketPipe.rb,v 1.1 1999/07/03 06:15:55 nakahiro Exp $
+RCS_ID = %q$Id: TCPSocketPipe.rb,v 1.2 1999/07/03 10:26:56 nakahiro Exp $
 
 require 'socket'
 require 'getopts'
@@ -228,3 +228,22 @@ EOM
 end
 
 main() if ( $0 == __FILE__ )
+
+=begin
+    TCPSocketPipe -- Creates I/O pipes for TCP socket tunneling.
+    Copyright (C) 1999  NAKAMURA, Hiroshi
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+=end
